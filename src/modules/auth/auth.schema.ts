153dceddaedf99bb1f538bb449529c9b87bfc16a
Razak <moabdulrazak17@gmail.com>
2022-09-30 00:37:26 +0000

    feat(schema): define login schema

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -42,4 +42,21 @@ export const registerSchema = {
 		})
 };
 
+export const loginSchema = {
+	body: z.object({
+		email: z
+			.string({
+				required_error: 'email is required'
+			})
+			.email('Not a valid email'),
+		password: z
+			.string({
+				required_error: 'password is required'
+			})
+			.min(6, 'password must be at least 6 characters')
+			.max(42, 'password must not be longer than 64 charcters')
+	})
+};
+
 export type RegisterUserBody = TypeOf<typeof registerSchema.body>;
+export type LoginBody = TypeOf<typeof loginSchema.body>;
