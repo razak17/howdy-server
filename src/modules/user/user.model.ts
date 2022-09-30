33248a3a8b6a0ffd7ca0688ad9f970e08658a24f
Razak <moabdulrazak17@gmail.com>
2022-09-30 00:08:12 +0000

    chore(models): create user model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,50 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
+import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export class Post extends TimeStamps {
+	@prop({ required: true })
+	public username: string;
+
+	@prop({ required: true })
+	public password: string;
+
+	@prop({ required: true })
+	public firstName: string;
+
+	@prop({ required: true })
+	public lastName: string;
+
+	@prop({ required: true })
+	public about: string;
+
+	@prop({ required: true })
+	public profilePicture: string;
+
+	@prop({ required: true })
+	public coverPicture: string;
+
+	@prop({ required: true })
+	public city: string;
+
+	@prop({ required: true })
+	public country: string;
+
+	@prop({ required: true })
+	public workplace: string;
+
+	@prop({ required: true })
+	public relationship: string;
+
+	@prop({ default: false })
+	public isAdmin: boolean;
+
+	@prop({ type: () => [String] })
+	public followers: string[];
+
+	@prop({ type: () => [String] })
+	public following: string[];
+}
+
+export const PostModel = getModelForClass(Post, {
+	schemaOptions: { timestamps: true }
+});
