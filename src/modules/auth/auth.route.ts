95d2bdff55d6bc4c7fa8f9f8065be3e89c204124
Razak <moabdulrazak17@gmail.com>
2022-09-30 00:49:31 +0000

    chore(auth): validate register route

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import { processRequestBody } from 'zod-express-middleware';
 import { helloController } from '../user/user.route';
 import { registerHandler } from './auth.controller';
+import { registerSchema } from './auth.schema';
 
 const router = express.Router();
 
 // Register
-router.post('/register', registerHandler);
+router.post('/register', processRequestBody(registerSchema.body), registerHandler);
 
 // Login
 router.post('/login', helloController);
